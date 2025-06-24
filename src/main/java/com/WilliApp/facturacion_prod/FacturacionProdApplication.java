01094ab48e4414ddfda227709c71ab9e3035e99b
wpazescandela <wi.paz@duocuc.cl>
2025-06-24 11:54:13 -0400

    Configuracion del proyecto, acceso a BDD y configuracion de Bean Model Mapper

---
+++ src/main/java/com/WilliApp/facturacion_prod/FacturacionProdApplication.java
@@ -0,0 +1,14 @@
+package com.WilliApp.facturacion_prod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionProdApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(FacturacionProdApplication.class, args);
+	}
+}
